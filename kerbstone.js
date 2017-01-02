25c7a7d472eb103a223662416c23af80ae26b7dd
sagittarius89 <piotr.zych@gmx.com>
2017-01-02 16:40:17 +0100

    Update kerbstone.js
    Rozwiązany problem z nie ładowaniem obrazków

--- kerbstone.js
+++ kerbstone.js
@@ -1,93 +1,89 @@
 // ==UserScript==
-// @name         Kerbstone
+// @name         Krawężnik
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  dodaje funkcje krawężnika do mirko
 // @author       @ZasilaczKomputerowy
 // @match        http://www.wykop.pl/mikroblog/*
+// @match        http://www.wykop.pl/link/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    function init()
-    {
-        // dodaje przycisk do watkow
-        $('#itemsStream').children().each(function(elem) {
-            var vC = $(this).find('p.vC').first();
-            var html = $(vC).html();
-            //console.log(html);
-            var id = $(this).find('div.wblock').attr('data-id');
+    // dodaje przycisk do watkow
+    $('#itemsStream').children().each(function(elem) {
+        var vC = $(this).find('p.vC').first();
+        var html = $(vC).html();
+        var id = $(this).find('div.wblock').attr('data-id');
 
+        if(localStorage.listaZwinietych === undefined)
+        {
+            localStorage.setItem("listaZwinietych", JSON.stringify([]));
+        }
+
+        var zwin = function(id)
+        {
+            var parent = $('div[data-id=' + id +' ]').first().parent();
+            $(parent).find('div.text').first().css('display', 'none');
+            $(parent).find('div.elements').first().css('display', 'none');
+            $(parent).find('img.avatar').first().css('display', 'none');
+            $(parent).children('ul.sub').first().css('display', 'none');
+            $(parent).find('#' + id + '_handle').first().text('[ + ]');
+        };
+
+        var rozwin = function(id)
+        {
+            var parent = $('div[data-id=' + id +' ]').first().parent();
+            $(parent).find('div.text').first().css('display', 'block');
+            $(parent).find('div.elements').first().css('display', 'block');
+            $(parent).find('img.avatar').first().css('display', 'block');
+            $(parent).children('ul.sub').first().css('display', 'block');
+            $(parent).find('#' + id + '_handle').first().text('[ - ]');
+        };
+
+        var listaZwinietych = JSON.parse(localStorage.getItem("listaZwinietych"));
+
+        var idHandle = id + "_handle";
+        if(listaZwinietych.indexOf(id) == -1)
+        {
+            html = '<span id="' + idHandle + '">[ - ]</span>' + html;
+            $(vC).html(html);
+        }
+        else
+        {
+            html = '<span id="' + id + '_handle">[ + ]</span>' + html;
+            $(vC).html(html);
+
+            zwin(id);
+        }
+
+        $(vC).find('#' + idHandle).click(function() {
             if(localStorage.listaZwinietych === undefined)
             {
                 localStorage.setItem("listaZwinietych", JSON.stringify([]));
             }
 
-            var zwin = function(id)
-            {
-                var parent = $('div[data-id=' + id +' ]').parent();
-                $(parent).find('div.text').first().css('display', 'none');
-                $(parent).find('div.elements').first().css('display', 'none');
-                $(parent).find('img.avatar').first().css('display', 'none');
-                $(parent).children('ul.sub').css('display', 'none');
-                $(parent).find('#' + id + '_handle').text('[ + ]');
-            };
-
-            var rozwin = function(id)
-            {
-                var parent = $('div[data-id=' + id +' ]').parent();
-                $(parent).find('div.text').first().css('display', 'block');
-                $(parent).find('div.elements').first().css('display', 'block');
-                $(parent).find('img.avatar').first().css('display', 'block');
-                $(parent).children('ul.sub').css('display', 'block');
-                $(parent).find('#' + id + '_handle').text('[ - ]');
-            };
-            
             var listaZwinietych = JSON.parse(localStorage.getItem("listaZwinietych"));
+            var idx = listaZwinietych.indexOf(id);
 
-            var idHandle = id + "_handle";
-            if(listaZwinietych.indexOf(id) == -1)
+            if(idx>-1)
             {
-                html = '<span id="' + idHandle + '">[ - ]</span>' + html;
-                $(vC).html(html);
+                listaZwinietych.splice(idx,1);
+
+                localStorage.setItem("listaZwinietych", JSON.stringify(listaZwinietych));
+
+                rozwin(id);
             }
             else
             {
-                html = '<span id="' + id + '_handle">[ + ]</span>' + html;
-                $(vC).html(html);
-                
+                listaZwinietych.push(id);
+
+                localStorage.setItem("listaZwinietych", JSON.stringify(listaZwinietych));
+
                 zwin(id);
             }
-            
-            $(vC).find('#' + idHandle).click(function() {
-                    if(localStorage.listaZwinietych === undefined)
-                    {
-                        localStorage.setItem("listaZwinietych", JSON.stringify([]));
-                    }
-
-                    var listaZwinietych = JSON.parse(localStorage.getItem("listaZwinietych"));
-                    var idx = listaZwinietych.indexOf(id);
-
-                    if(idx>-1)
-                    {
-                        listaZwinietych.splice(idx,1);
-
-                        localStorage.setItem("listaZwinietych", JSON.stringify(listaZwinietych));
-
-                        rozwin(id);
-                    }
-                    else
-                    {
-                        listaZwinietych.push(id);
-
-                        localStorage.setItem("listaZwinietych", JSON.stringify(listaZwinietych));
-
-                        zwin(id);
-                    }
-             });
-	
         });
-    }
-    
-    init();
+
+        $('img[data-original]').each(function() { $(this).attr('src', $(this).attr('data-original')); });
+    });
 })();
